62bf5060d9df79ccb05cf987212516fc915e4a2a
marinib <marin1704@gmail.com>
2017-04-14 11:08:19 -0400

    updated code simplified and condensed

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // starts with a printout/prompt that will only be displayed once
+        // initializing a few variables which need to be declared only once)
+        // TODO: place math.random methods in their own class
 
         System.out.println("Welcome to the Casino!");
         String rollAgain = "";
-        int count = 1;
+        int rollCount = 1;
 
+        // start of the do/while loop (will be later transferred into its own class)
         do {
             Scanner scan = new Scanner(System.in);
             System.out.println("How many faces will your dice have?");
             int diceFaces = scan.nextInt();
             System.out.println("Great! You have selected " +diceFaces+ " faced dice.");
 
-            int die1 = (int)(Math.random()* diceFaces);
-            int die2 = (int)(Math.random()* diceFaces);
+            int die1 = (int)(Math.random()* diceFaces+1); // random for die1 (updated to compensate for multiplying by 0)
+            int die2 = (int)(Math.random()* diceFaces+1); // random for die2 (updated for same reason)
 
-            int rollCount = count ++;
-            System.out.println("Roll: " + rollCount);
+            // this is where "Roll: (number)" begins and printed out
+            System.out.println("Roll: " + rollCount++); // simplified and condensed
             System.out.println(die1);
             System.out.println(die2);
 
@@ -26,6 +30,7 @@ public class Main {
 
             rollAgain = scan.next();
         }
+        // end of while loop and starts loop again if answer is "y"
         while (rollAgain.equals("y"));
     }
 }
